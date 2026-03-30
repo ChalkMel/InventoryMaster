53d881a84d2b0d74386cc9938e4c8470b87b0e8b
ChalkMel <melnikvv24@st.ithub.ru>
2026-03-30 11:41:04 +0300

    2

--- My project/Assets/_Source/Effects/ItemEffect.cs
+++ My project/Assets/_Source/Effects/ItemEffect.cs
@@ -4,14 +4,4 @@ using UnityEngine;
 public abstract class ItemEffect : ScriptableObject
 {
   public abstract void OnReceive(Inventory inventory);
-
-  public void OnUse(Inventory inventory, ItemData item)
-  {
-    throw new NotImplementedException();
-  }
-
-  public void OnDrop(Inventory inventory, ItemData item)
-  {
-    throw new NotImplementedException();
-  }
 }
